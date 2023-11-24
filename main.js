90cf80880aee8b352a6908ed9fcce3393adc3348
MapLyz <151870826+MapLyz@users.noreply.github.com>
2023-11-24 19:01:43 +0200

    preview-1.0

---
+++ main.js
@@ -0,0 +1,65 @@
+
+// Javascript file
+
+
+let menupc = document.querySelector('.menu-open-pc');
+let informatii = document.querySelector('.info-pc');
+let even = document.querySelector('.event-pc');
+let linkContainer = document.querySelector('.main-pc-menu');
+
+informatii.addEventListener('click', function(event) {
+  event.stopPropagation();
+  menupc.style.display = 'block';
+
+  linkContainer.innerHTML = `
+  <a href="">Informatii despre site</a>
+  <a href="">Informatii despre Liceu</a>
+  <a href="">Contacteaza-ne</a>
+  <a href="">Vezi regulile scoi</a>
+  `;
+});
+
+even.addEventListener('click', function(event) {
+  event.stopPropagation();
+  menupc.style.display = 'block';
+  linkContainer.innerHTML = `
+    Nici un eveniment momentan
+  `;
+});
+
+document.addEventListener('click', function(event) {
+  menupc.style.display = 'none';
+});
+
+
+let searchbtn,menubtn,close,menu,wrc,wrm;
+
+  searchbtn = document.querySelector('.search')
+  menubtn = document.querySelector('.menu-p')
+  menu = document.querySelector('.menu-open')
+  close = document.querySelector('.close')
+  wrc = document.querySelector('.close-w')
+  wrm = document.querySelector('.warnnig')
+
+  searchbtn.addEventListener('click', ()=> {
+    window.location.replace("search.html");
+  })
+
+  menubtn.addEventListener('click', ()=> {
+
+    menu.style.display = 'block'
+
+  })
+
+  close.addEventListener('click', ()=> {
+
+    menu.style.display = 'none'
+
+  })
+  wrc.addEventListener('click', ()=> {
+
+    wrm.style.display = 'none'
+
+  })
+
+
